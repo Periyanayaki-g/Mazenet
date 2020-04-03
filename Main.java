b6e86d2248c6dc50a1a64b36d0c9a6ad02f7cbe3
periyanayaki-g <periyanayaki76@gmail.com>
2020-04-03 15:53:38 +0530

    Main

---
+++ Main.java
@@ -0,0 +1,57 @@
+class Main 
+{
+    
+static int i=20;
+    
+static int e,f=20,g=40,h,k,c=60,y;
+    
+static int j=10;
+    
+public static void m1()
+    
+{
+    
+e=i+j;
+    
+System.out.println(e);
+    
+}
+    
+public static void m2() 
+    
+{
+    
+g=e-f;
+    
+System.out.println(g);
+    
+}
+    
+public static void m3()
+    
+{
+    
+k=g*h;
+    
+System.out.println(k);
+    
+}
+   
+ public static void m4()
+    
+{
+    
+y=c/k;
+    
+System.out.println(y);
+    
+}
+    
+public static void main(String args[])
+    
+{
+       
+ System.out.println(e+g+k+y);
+    
+}
+}
